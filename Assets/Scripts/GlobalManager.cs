01eebdd8ae822a43bcae964cabeacd2a973f7988
BH67Unity <107767710+BH67Unity@users.noreply.github.com>
2022-06-20 15:54:20 -0400

    Alpha 0.1
    Menu UI arranged. Global manager implemented. Start and Exit functionality.

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//required for buttons and TextMeshPro
+using System.IO;//required for JSON serialization
+using UnityEngine.SceneManagement;//required for scene transitions
+using UnityEditor;//required for conditional compiling of exit functionality
+[DefaultExecutionOrder(1000)]//because globabl UI is involved, this ensures the script loads last
+
+public class GlobalManager : MonoBehaviour
+{
+    public static GlobalManager instance;
+
+    public GameObject playerNameInput;
+    public string playerName;
+    public GameObject startButton;
+    public GameObject exitButton;
+    public GameObject resetHiScoreButton;
+
+    void IsDuplicateSingleton()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(instance);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //apply singleton pattern to global manager
+        IsDuplicateSingleton();
+
+        //link to menu UI elements
+        playerNameInput = GameObject.Find("Player InputField");
+        startButton = GameObject.Find("Start Button");
+        exitButton = GameObject.Find("Exit Button");
+        resetHiScoreButton = GameObject.Find("Reset HiScore Button");
+
+        //wire up button click events
+        startButton.GetComponent<Button>().onClick.AddListener(StartClicked);
+        exitButton.GetComponent<Button>().onClick.AddListener(ExitClicked);
+        resetHiScoreButton.GetComponent<Button>().onClick.AddListener(ResetClicked);
+    }
+    void StartClicked()
+    {
+        SceneManager.LoadScene("main");
+    }
+    void ExitClicked()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+    void ResetClicked()
+    {
+
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UI;//required for buttons and TextMeshPro
 
 public class MainManager : MonoBehaviour
 {
@@ -11,21 +10,25 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
-    public GameObject GameOverText;
-    
+    public GameObject GameOverText1;
+    public GameObject GameOverText2;
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
     // Start is called before the first frame update
     void Start()
     {
+        PopulateBricks();
+    }
+    void PopulateBricks()
+    {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -57,7 +60,7 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                GlobalManager.instance.RestartGame();
             }
         }
     }
@@ -71,6 +74,7 @@ public class MainManager : MonoBehaviour
     public void GameOver()
     {
         m_GameOver = true;
-        GameOverText.SetActive(true);
+        GameOverText1.SetActive(true);
+        GameOverText2.SetActive(true);
     }
 }
